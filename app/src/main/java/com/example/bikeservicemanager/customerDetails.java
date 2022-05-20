960691721db6efc8ab2f98c429f721d644f3f41c
nandinisangar <nandinisangar6@gmail.com>
2022-05-21 01:56:52 +0530

    final commit

--- app/src/main/java/com/example/bikeservicemanager/customerDetails.java
+++ app/src/main/java/com/example/bikeservicemanager/customerDetails.java
@@ -19,7 +19,7 @@ public class customerDetails extends AppCompatActivity {
     String[] languages = {"Hero", "Honda", "Yamaha", "Kawasaki", "TVS", "Bajaj", "Royal Enfield", "Suzuki"};
     private EditText txt_name, txt_contact, txt_Bike_Manufacturer, txt_Bike_model, txt_Build_Year, txt_Bike_No, txt_Daily_Running, txt_address;
     private Button add;
-    private MySQLiteOpenHelper mySQLiteOpenHelper;
+   // private MySQLiteOpenHelper mySQLiteOpenHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +44,7 @@ public class customerDetails extends AppCompatActivity {
         txt_Daily_Running = findViewById(R.id.editTextdailyrunning);
         txt_address = findViewById(R.id.editTextaddress);
         add = findViewById(R.id.btnadd);
-        mySQLiteOpenHelper = new MySQLiteOpenHelper(customerDetails.this);
+        //mySQLiteOpenHelper = new MySQLiteOpenHelper(customerDetails.this);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,7 +59,6 @@ public class customerDetails extends AppCompatActivity {
 
                 if (name.isEmpty() && contact.isEmpty() && bm.isEmpty() && bmodel.isEmpty() && byear.isEmpty() && bno.isEmpty() && dailyrunning.isEmpty() && address.isEmpty()) {
                     Toast.makeText(customerDetails.this, "Please enter all the Filed", Toast.LENGTH_SHORT).show();
-                    023EW
                 }
 
                 CustomerDetailsHelper customerDetailsHelper = new CustomerDetailsHelper(
